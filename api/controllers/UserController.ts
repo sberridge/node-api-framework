e6181ee02f469c31fbdb91d21bf10c60bca4d88c
Steven Berridge <bez.berridge@gmail.com>
2022-03-27 23:15:15 +0100

    added pagination functionality to controllers

--- api/controllers/UserController.ts
+++ api/controllers/UserController.ts
@@ -4,6 +4,8 @@ import { ResponseGenerator } from './../library/ResponseGenerator';
 import {User} from '../models/User';
 import {WSControl} from './../library/websockets/WSControl';
 import {JWT} from './../library/authentication/JWT';
+import { paginationURLQuery } from './types/ControllerTypes';
+import { applyQueryPagination } from './functions/ControllerFunctions';
 const ws:WSControl = WSControl.getInstance();
 
 
@@ -15,16 +17,27 @@ exports.fake_auth = async function(req:Request, res:Response, next:NextFunction)
     next();
 };
 
-exports.get_users = async function(req:Request, res:Response, next:NextFunction) {
+exports.get_users = async function(req:Request<{},{},{},paginationURLQuery>, res:Response, next:NextFunction) {
+
+    const error = ()=>{
+        return res.json(ResponseGenerator.failure("Something went wrong fetching the users list"));
+    }
+
+    const {query} = req;
     const authData = JWT.getInstance().verify(req);
     if(authData) {
         ws.send(authData['user_id'], {
             "message": "you called get_users"
         });
     }
+
     let getUsers = (new User()).all();
-    let pagination  = await getUsers.paginate(10,1);
-    let totalRows = pagination.total_rows;
+    const paginationResult = await applyQueryPagination(getUsers, query);
+    if(!paginationResult.success) {
+        return error();
+    }
+    
+    let totalRows = paginationResult.result.total_rows;
     res.json(ResponseGenerator.success((await getUsers.fetchModels()).toJSON(), totalRows));
     next();
 };
---
+++ api/controllers/types/ControllerTypes.ts
@@ -0,0 +1,4 @@
+export type paginationURLQuery = {
+    "page_number": string
+    "per_page": string
+}
